277e46e56beddd270f94b4131e7b27334aed88bb
liuxl <ylulu2007@sina.com>
2018-04-25 15:36:05 +0800

    加病人信息查询

--- TongliApi/src/main/java/com/realdoctor/back/dal/vo/OutpatientInfoVo.java
+++ TongliApi/src/main/java/com/realdoctor/back/dal/vo/OutpatientInfoVo.java
@@ -10,21 +10,55 @@ import com.easyway.business.framework.pojo.BasePojo;
  */
 public class OutpatientInfoVo extends BasePojo {
 
+    /**
+     * 
+     */
     private static final long serialVersionUID = 1L;
+    
     // 病人ID
     private String            patientId;
+    // 姓名
+    private String            name;
+    // 性别
+    private String            sexCode;
+    // 出生日期
+    private Date              birthDate;
+    // 身份证类型
+    private String            idTypeCode;
+    // 证件号码
+    private String            idNo;
+    // 电话号码
+    private String            telNo;
+    // 国籍
+    private String            nationalityCode;
+    // 血型
+    private String            aboCode;
+    // RH血型
+    private String            rhCode;
+    // 婚姻状况
+    private String            marriageCode;
+    // 过敏标识
+    private String            drugAllergyMark;
     // 就诊日期时间
     private Date              visitDtime;
     // 就诊机构名称
     private String            visitOrgName;
     // 就诊科室名称
     private String            visitDeptName;
+    // 门诊诊断代码
+    private String            outpatDiagCode;
     // 门诊诊断名称
     private String            outpatDiagName;
     // 责任医师姓名
     private String            respDoctorName;
     // 药物名称
     private String            drugName;
+    // 药物剂型代码
+    private String            drugFormCode;
+    // 药物标准编码
+    private String            drugStdCode;
+    // 药物标准名称
+    private String            drugStdName;
     // 用药天数
     private Integer           drugUsingDays;
     // 药物使用频率
@@ -58,6 +92,94 @@ public class OutpatientInfoVo extends BasePojo {
         this.patientId = patientId;
     }
     
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getSexCode() {
+        return sexCode;
+    }
+    
+    public void setSexCode(String sexCode) {
+        this.sexCode = sexCode;
+    }
+    
+    public Date getBirthDate() {
+        return birthDate;
+    }
+    
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+    
+    public String getIdTypeCode() {
+        return idTypeCode;
+    }
+    
+    public void setIdTypeCode(String idTypeCode) {
+        this.idTypeCode = idTypeCode;
+    }
+    
+    public String getIdNo() {
+        return idNo;
+    }
+    
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+    
+    public String getTelNo() {
+        return telNo;
+    }
+    
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
+    
+    public String getNationalityCode() {
+        return nationalityCode;
+    }
+    
+    public void setNationalityCode(String nationalityCode) {
+        this.nationalityCode = nationalityCode;
+    }
+    
+    public String getAboCode() {
+        return aboCode;
+    }
+    
+    public void setAboCode(String aboCode) {
+        this.aboCode = aboCode;
+    }
+    
+    public String getRhCode() {
+        return rhCode;
+    }
+    
+    public void setRhCode(String rhCode) {
+        this.rhCode = rhCode;
+    }
+    
+    public String getMarriageCode() {
+        return marriageCode;
+    }
+    
+    public void setMarriageCode(String marriageCode) {
+        this.marriageCode = marriageCode;
+    }
+    
+    public String getDrugAllergyMark() {
+        return drugAllergyMark;
+    }
+    
+    public void setDrugAllergyMark(String drugAllergyMark) {
+        this.drugAllergyMark = drugAllergyMark;
+    }
+    
     public Date getVisitDtime() {
         return visitDtime;
     }
@@ -82,6 +204,14 @@ public class OutpatientInfoVo extends BasePojo {
         this.visitDeptName = visitDeptName;
     }
     
+    public String getOutpatDiagCode() {
+        return outpatDiagCode;
+    }
+    
+    public void setOutpatDiagCode(String outpatDiagCode) {
+        this.outpatDiagCode = outpatDiagCode;
+    }
+    
     public String getOutpatDiagName() {
         return outpatDiagName;
     }
@@ -104,6 +234,30 @@ public class OutpatientInfoVo extends BasePojo {
     
     public void setDrugName(String drugName) {
         this.drugName = drugName;
+    }
+    
+    public String getDrugFormCode() {
+        return drugFormCode;
+    }
+    
+    public void setDrugFormCode(String drugFormCode) {
+        this.drugFormCode = drugFormCode;
+    }
+    
+    public String getDrugStdCode() {
+        return drugStdCode;
+    }
+    
+    public void setDrugStdCode(String drugStdCode) {
+        this.drugStdCode = drugStdCode;
+    }
+    
+    public String getDrugStdName() {
+        return drugStdName;
+    }
+    
+    public void setDrugStdName(String drugStdName) {
+        this.drugStdName = drugStdName;
     }
     
     public Integer getDrugUsingDays() {
--- TongliApi/src/main/java/com/realdoctor/back/web/OutpatientController.java
+++ TongliApi/src/main/java/com/realdoctor/back/web/OutpatientController.java
@@ -1,8 +1,13 @@
 package com.realdoctor.back.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.easyway.business.framework.mybatis.annotion.SingleValue;
+import com.easyway.business.framework.pojo.Grid;
 import com.easyway.business.framework.springmvc.controller.CrudController;
+import com.easyway.business.framework.springmvc.result.ResultBody;
+import com.easyway.business.framework.springmvc.result.ResultUtil;
 import com.realdoctor.back.dal.pojo.Outpatient;
 import com.realdoctor.back.service.OutpatientBo;
 
@@ -11,7 +16,46 @@ import com.realdoctor.back.service.OutpatientBo;
  * 
  * @author xl.liu
  */
-@RestController
+@RestController(value = "/patient")
 public class OutpatientController extends CrudController<Outpatient, OutpatientBo> {
 
+    @GetMapping
+    public ResultBody list(OutpatientQuery query) throws Exception {
+        return ResultUtil.success(this.bo.queryFrontList(query));
+    }
+
+    public static class OutpatientQuery extends Grid {
+
+        private String beginDate;
+        private String endDate;
+        private String mobilePhone;
+
+        @SingleValue(tableAlias = "t0", column = "visit_dtime", equal = ">=")
+        public String getBeginDate() {
+            return beginDate;
+        }
+
+        public void setBeginDate(String beginDate) {
+            this.beginDate = beginDate;
+        }
+
+        @SingleValue(tableAlias = "t0", column = "visit_dtime", equal = "<=")
+        public String getEndDate() {
+            return endDate;
+        }
+
+        public void setEndDate(String endDate) {
+            this.endDate = endDate;
+        }
+
+        @SingleValue(column = "tel_no", equal = "=")
+        public String getMobilePhone() {
+            return mobilePhone;
+        }
+
+        public void setMobilePhone(String mobilePhone) {
+            this.mobilePhone = mobilePhone;
+        }
+
+    }
 }
