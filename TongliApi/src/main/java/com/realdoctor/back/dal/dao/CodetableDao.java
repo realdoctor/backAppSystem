9d7917b30020276d1b6566a2278f928f346107bb
liuxl <ylulu2007@sina.com>
2018-04-26 15:18:58 +0800

    加入基础数据缓存

--- TongliApi/src/main/java/com/realdoctor/back/dal/dao/CodetableDao.java
+++ TongliApi/src/main/java/com/realdoctor/back/dal/dao/CodetableDao.java
@@ -1,8 +1,12 @@
 package com.realdoctor.back.dal.dao;
 
-import com.realdoctor.back.dal.pojo.Codetable;
+import java.util.List;
+import java.util.Map;
+
 import com.easyway.business.framework.dao.CrudDao;
+import com.realdoctor.back.dal.pojo.Codetable;
 
 public interface CodetableDao extends CrudDao<Codetable> {
 
+    public List<Map<String, Object>> findSectionList(String name);
 }
--- TongliApi/src/main/java/com/realdoctor/back/service/CodetableBo.java
+++ TongliApi/src/main/java/com/realdoctor/back/service/CodetableBo.java
@@ -1,11 +1,24 @@
 package com.realdoctor.back.service;
 
-import com.realdoctor.back.dal.pojo.Codetable;
-import com.easyway.business.framework.bo.CrudBo;
-import com.realdoctor.back.dal.dao.CodetableDao;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import com.easyway.business.framework.bo.CrudBo;
+import com.realdoctor.back.dal.dao.CodetableDao;
+import com.realdoctor.back.dal.pojo.Codetable;
+import com.realdoctor.exception.DBException;
+
 @Service
-public class CodetableBo extends CrudBo<Codetable,CodetableDao> {
+public class CodetableBo extends CrudBo<Codetable, CodetableDao> {
+
+    public List<Map<String, Object>> findSectionList(String name) {
+        try {
+            return this.dao.findSectionList(name);
+        } catch (Exception ex) {
+            throw new DBException(ex);
+        }
+    }
 
 }
