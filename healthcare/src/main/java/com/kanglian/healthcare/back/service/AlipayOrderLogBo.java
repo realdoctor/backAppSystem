5ecab8039a3b8c8b26b273bfb4e89e46bf59104a
liuxl <ylulu2007@sina.com>
2018-05-09 14:30:56 +0800

    插入支付宝日志

--- healthcare/src/main/java/com/kanglian/healthcare/back/service/AlipayOrderLogBo.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/AlipayOrderLogBo.java
@@ -1,11 +1,32 @@
 package com.kanglian.healthcare.back.service;
 
-import com.easyway.business.framework.bo.CrudBo;
-import com.kanglian.healthcare.back.dal.dao.AlipayOrderLogDao;
 import org.springframework.stereotype.Service;
+import com.easyway.business.framework.bo.CrudBo;
+import com.easyway.business.framework.util.DateUtil;
+import com.kanglian.healthcare.back.dal.dao.AlipayOrderLogDao;
 import com.kanglian.healthcare.back.dal.pojo.AlipayOrderLog;
+import com.kanglian.healthcare.exception.DBException;
 
 @Service
-public class AlipayOrderLogBo extends CrudBo<AlipayOrderLog,AlipayOrderLogDao> {
+public class AlipayOrderLogBo extends CrudBo<AlipayOrderLog, AlipayOrderLogDao> {
 
+    /**
+     * 写入支付宝预付单日志
+     * 
+     * @param userId
+     * @param orderNo
+     * @param orderString
+     */
+    public void insertPayOrderLog(String userId, String orderNo, String orderString) {
+        try {
+            AlipayOrderLog alipayOrderLog = new AlipayOrderLog();
+            alipayOrderLog.setUserId(userId);
+            alipayOrderLog.setOrderNo(orderNo);
+            alipayOrderLog.setOrderString(orderString);
+            alipayOrderLog.setAddTime(DateUtil.currentDate());
+            this.dao.save(alipayOrderLog);
+        } catch (Exception ex) {
+            throw new DBException(ex);
+        }
+    }
 }
