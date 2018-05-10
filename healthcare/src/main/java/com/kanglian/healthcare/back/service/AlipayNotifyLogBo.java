47fe3d582a6f1079b57831fc6611599e1489b58f
liuxl <ylulu2007@sina.com>
2018-05-10 18:01:45 +0800

    写入支付宝回调日志

--- healthcare/src/main/java/com/kanglian/healthcare/back/service/AlipayNotifyLogBo.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/AlipayNotifyLogBo.java
@@ -1,13 +1,14 @@
 package com.kanglian.healthcare.back.service;
 
+import java.util.Map;
+import org.springframework.stereotype.Service;
+import com.easyway.business.framework.bo.CrudBo;
+import com.easyway.business.framework.util.DateUtil;
 import com.kanglian.healthcare.back.constants.AlipayNotifyResponse;
 import com.kanglian.healthcare.back.dal.dao.AlipayNotifyLogDao;
 import com.kanglian.healthcare.back.dal.pojo.AlipayNotifyLog;
-import com.alibaba.fastjson.JSON;
-import com.easyway.business.framework.bo.CrudBo;
-import com.easyway.business.framework.util.DateUtil;
-import java.util.Map;
-import org.springframework.stereotype.Service;
+import com.kanglian.healthcare.util.JsonUtil;
+import com.kanglian.healthcare.util.PayCommonUtil;
 
 @Service
 public class AlipayNotifyLogBo extends CrudBo<AlipayNotifyLog, AlipayNotifyLogDao> {
@@ -18,11 +19,18 @@ public class AlipayNotifyLogBo extends CrudBo<AlipayNotifyLog, AlipayNotifyLogDa
      * @param alipayResponse
      */
     public void insertNotifyLog(AlipayNotifyResponse alipayResponse) {
-        Map<String, String> payNotifyResponse = alipayResponse.getAlipay_trade_app_pay_response();
+        Map<String, String> response = alipayResponse.getAlipay_trade_app_pay_response();
+        String userId = null;
+        if (response.get("passback_params") != null) {
+            // 附加数据
+            String passback_params = PayCommonUtil.urlDecodeUTF8(response.get("passback_params"));
+            Map<String, Object> params = JsonUtil.jsonToMap(passback_params);
+            userId = params.get("userId") + "";
+        }
         AlipayNotifyLog alipayNotifyLog = new AlipayNotifyLog();
-        alipayNotifyLog.setUserId(null);
-        alipayNotifyLog.setOrderNo(payNotifyResponse.get("out_trade_no"));
-        alipayNotifyLog.setNotifyString(JSON.toJSONString(alipayResponse));
+        alipayNotifyLog.setUserId(userId);
+        alipayNotifyLog.setOrderNo(response.get("out_trade_no"));
+        alipayNotifyLog.setNotifyString(JsonUtil.beanToJson(alipayResponse));
         alipayNotifyLog.setAddTime(DateUtil.currentDate());
         dao.save(alipayNotifyLog);
     }
