2c2e077b7c4af2f71a1f954cba809bdedeed979e
liuxl <ylulu2007@sina.com>
2018-05-14 15:11:17 +0800

    刷新字典码表task

---
+++ healthcare/src/main/java/com/kanglian/healthcare/quartz/task/CodeRefreshTask.java
@@ -0,0 +1,41 @@
+package com.kanglian.healthcare.quartz.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.kanglian.healthcare.back.service.CodetableBo;
+import com.kanglian.healthcare.quartz.common.AbstractTask;
+
+/**
+ * 字段码表刷新
+ * 
+ * @author xl.liu
+ */
+public class CodeRefreshTask extends AbstractTask {
+
+    /** logger */
+    private static final Logger logger = LoggerFactory.getLogger(CodeRefreshTask.class);
+
+    @Autowired
+    private CodetableBo         codetableBo;
+
+    public CodeRefreshTask() {
+        this.taskName = "字典码表刷新";
+    }
+
+    @Override
+    public boolean canProcess() {
+        return true;
+    }
+
+    @Override
+    public void process() {
+        try {
+            logger.info("==============进入字典码表刷新");
+            codetableBo.initCacheData();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+}
