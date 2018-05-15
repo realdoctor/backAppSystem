9dd63cfd8d495a16c2df30dbc2806d3cea5b9926
liuxl <ylulu2007@sina.com>
2018-05-15 15:33:41 +0800

    Add Log.

--- healthcare/src/main/java/com/kanglian/healthcare/back/web/UserController.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/web/UserController.java
@@ -240,19 +240,21 @@ public class UserController extends CrudController<User, UserBo> {
         if (user == null) {
             return ResultUtil.error("刷新token失败");
         }
+        final String mobilePhone = user.getMobilePhone();
         // 重新生成Token，利用redis过期缓存，一天刷一次。
         Object token = redisCacheManager.getCacheObject(
                 Constants.MARK_REFRESH_TOKEN_KEY_PREFIX.concat(String.valueOf(user.getUserId())));
         Map<String, Object> resultMap = new HashMap<String, Object>();
         resultMap.put("token", token);
+        logger.debug("================12小时前已刷过token="+token);
         if (token == null) {
-            token = JwtUtil.generToken(user.getMobilePhone(), JsonUtil.beanToJson(user),
-                    JwtUtil.JWT_TTL);
-            redisTokenManager.createRelationship(user.getMobilePhone(), String.valueOf(token));
+            token = JwtUtil.generToken(mobilePhone, JsonUtil.beanToJson(user), JwtUtil.JWT_TTL);
+            redisTokenManager.createRelationship(mobilePhone, String.valueOf(token));
             // 12小时过期
             redisCacheManager.setCacheObject(Constants.MARK_REFRESH_TOKEN_KEY_PREFIX
                     .concat(String.valueOf(user.getUserId())), token, 12L, TimeUnit.HOURS);
             resultMap.put("token", token);
+            logger.info("================手机号{}，重新生成token={}", new Object[] {mobilePhone, token});
         }
         return ResultUtil.success(resultMap);
     }
