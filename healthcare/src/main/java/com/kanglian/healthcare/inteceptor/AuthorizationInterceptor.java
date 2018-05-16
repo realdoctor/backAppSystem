c20a9af8ad34549d288aa8ef0392c774d5b31986
liuxl <ylulu2007@sina.com>
2018-05-16 10:34:33 +0800

    Add Log

--- healthcare/src/main/java/com/kanglian/healthcare/inteceptor/AuthorizationInterceptor.java
+++ healthcare/src/main/java/com/kanglian/healthcare/inteceptor/AuthorizationInterceptor.java
@@ -43,6 +43,7 @@ public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
         Method method = handlerMethod.getMethod();
         // 从header中得到token
         String token = request.getHeader(Constants.AUTHORIZATION);
+        logger.debug("============进入请求方法：{}", method.getName());
         logger.info("=================对请求进行身份验证，token="+token);
         if (token != null && token.length() > 0) {
             // 验证token
