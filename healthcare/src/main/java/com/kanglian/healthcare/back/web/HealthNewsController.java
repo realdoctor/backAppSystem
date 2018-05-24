a5620b90d41b206f1236201e970e2583e6a16207
liuxl <ylulu2007@sina.com>
2018-05-24 14:26:01 +0800

    增加详情

--- healthcare/src/main/java/com/kanglian/healthcare/back/web/HealthNewsController.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/web/HealthNewsController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.easyway.business.framework.pojo.Grid;
 import com.easyway.business.framework.springmvc.controller.CrudController;
 import com.easyway.business.framework.springmvc.result.ResultBody;
+import com.easyway.business.framework.springmvc.result.ResultUtil;
+import com.easyway.business.framework.util.StringUtil;
 import com.kanglian.healthcare.back.dal.pojo.HealthNews;
 import com.kanglian.healthcare.back.service.HealthNewsBo;
+import com.kanglian.healthcare.exception.InvalidParamException;
 
 /**
  * 健康资讯
@@ -21,6 +24,14 @@ public class HealthNewsController extends CrudController<HealthNews, HealthNewsB
     @GetMapping
     public ResultBody list(BaseQuery query) throws Exception {
         return super.list(query);
+    }
+    
+    @GetMapping("/info")
+    public ResultBody newsInfo(String newsId) throws Exception {
+        if (StringUtil.isEmpty(newsId)) {
+            throw new InvalidParamException("newsId");
+        }
+        return ResultUtil.success(this.bo.get(Integer.valueOf(newsId)));
     }
     
     public static class BaseQuery extends Grid {
