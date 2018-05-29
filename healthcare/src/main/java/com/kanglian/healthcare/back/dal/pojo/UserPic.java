f4bc9de8c14e10e2899fdc544ff6723b6827db03
liuxl <ylulu2007@sina.com>
2018-05-29 10:44:34 +0800

    用户头像

---
+++ healthcare/src/main/java/com/kanglian/healthcare/back/dal/pojo/UserPic.java
@@ -0,0 +1,50 @@
+package com.kanglian.healthcare.back.dal.pojo;
+
+import java.util.Date;
+import com.easyway.business.framework.pojo.BasePojo;
+
+public class UserPic extends BasePojo {
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private Long userId;
+	private String pic;
+	private String thumbnailPic;
+	private Date addTime;
+	private Date lastUpdateDtime;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+	public String getThumbnailPic() {
+		return thumbnailPic;
+	}
+	public void setThumbnailPic(String thumbnailPic) {
+		this.thumbnailPic = thumbnailPic;
+	}
+	public Date getAddTime() {
+		return addTime;
+	}
+	public void setAddTime(Date addTime) {
+		this.addTime = addTime;
+	}
+	public Date getLastUpdateDtime() {
+		return lastUpdateDtime;
+	}
+	public void setLastUpdateDtime(Date lastUpdateDtime) {
+		this.lastUpdateDtime = lastUpdateDtime;
+	}
+}
---
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/UserPicBo.java
@@ -0,0 +1,11 @@
+package com.kanglian.healthcare.back.service;
+
+import com.kanglian.healthcare.back.dal.dao.UserPicDao;
+import com.easyway.business.framework.bo.CrudBo;
+import org.springframework.stereotype.Service;
+import com.kanglian.healthcare.back.dal.pojo.UserPic;
+
+@Service
+public class UserPicBo extends CrudBo<UserPic,UserPicDao> {
+
+}
