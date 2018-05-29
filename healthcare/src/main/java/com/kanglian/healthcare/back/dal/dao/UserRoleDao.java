0bf24a1191c11263401d3ba64781ad2d75be93cf
liuxl <ylulu2007@sina.com>
2018-05-29 16:58:21 +0800

    角色关系表

---
+++ healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/UserRoleDao.java
@@ -0,0 +1,8 @@
+package com.kanglian.healthcare.back.dal.dao;
+
+import com.kanglian.healthcare.back.dal.pojo.UserRole;
+import com.easyway.business.framework.dao.CrudDao;
+
+public interface UserRoleDao extends CrudDao<UserRole> {
+
+}
---
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/UserRoleBo.java
@@ -0,0 +1,11 @@
+package com.kanglian.healthcare.back.service;
+
+import com.kanglian.healthcare.back.dal.pojo.UserRole;
+import com.kanglian.healthcare.back.dal.dao.UserRoleDao;
+import com.easyway.business.framework.bo.CrudBo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRoleBo extends CrudBo<UserRole,UserRoleDao> {
+
+}
