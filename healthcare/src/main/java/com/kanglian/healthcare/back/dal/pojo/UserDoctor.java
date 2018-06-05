97ff7d54294625b75b9176c3d6628c846a1cc5d6
liuxl <ylulu2007@sina.com>
2018-06-05 14:21:52 +0800

    用户医生类

---
+++ healthcare/src/main/java/com/kanglian/healthcare/back/dal/pojo/UserDoctor.java
@@ -0,0 +1,21 @@
+package com.kanglian.healthcare.back.dal.pojo;
+
+import com.easyway.business.framework.pojo.BasePojo;
+
+public class UserDoctor extends BasePojo {
+	private static final long serialVersionUID = 1L;
+	private Integer userId;
+	private String doctorCode;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getDoctorCode() {
+		return doctorCode;
+	}
+	public void setDoctorCode(String doctorCode) {
+		this.doctorCode = doctorCode;
+	}
+}
---
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/UserDoctorBo.java
@@ -0,0 +1,11 @@
+package com.kanglian.healthcare.back.service;
+
+import com.kanglian.healthcare.back.dal.pojo.UserDoctor;
+import com.easyway.business.framework.bo.CrudBo;
+import org.springframework.stereotype.Service;
+import com.kanglian.healthcare.back.dal.dao.UserDoctorDao;
+
+@Service
+public class UserDoctorBo extends CrudBo<UserDoctor,UserDoctorDao> {
+
+}
