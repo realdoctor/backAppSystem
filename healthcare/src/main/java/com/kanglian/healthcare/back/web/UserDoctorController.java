a69360d3b4ef063eecc02537edefaed4c6009639
liuxl <ylulu2007@sina.com>
2018-06-05 14:55:41 +0800

    加入头像显示

--- healthcare/src/main/java/com/kanglian/healthcare/back/web/UserDoctorController.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/web/UserDoctorController.java
@@ -4,16 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSONObject;
+import com.easyway.business.framework.json.JsonClothProcessor;
 import com.easyway.business.framework.mybatis.annotion.SingleValue;
 import com.easyway.business.framework.pojo.Grid;
 import com.easyway.business.framework.springmvc.controller.CrudController;
 import com.easyway.business.framework.springmvc.result.ResultBody;
 import com.easyway.business.framework.springmvc.result.ResultUtil;
+import com.github.pagehelper.util.StringUtil;
 import com.kanglian.healthcare.authorization.annotation.CurrentUser;
+import com.kanglian.healthcare.back.constants.Constants;
 import com.kanglian.healthcare.back.dal.pojo.User;
 import com.kanglian.healthcare.back.service.HospitalGuahaoLogBo;
 import com.kanglian.healthcare.back.service.UserBo;
 import com.kanglian.healthcare.exception.InvalidParamException;
+import com.kanglian.healthcare.util.PropConfig;
 
 @RestController
 @RequestMapping(value = "/user/doctor")
@@ -34,7 +39,25 @@ public class UserDoctorController extends CrudController<User, UserBo> {
         if (user.getUserId() == null) {
             throw new InvalidParamException("userId");
         }
-        return ResultUtil.success(hospitalGuahaoLogBo.myPatientOrder(query));
+        return ResultUtil.success(hospitalGuahaoLogBo.myPatientOrder(query),
+                new JsonClothProcessor() {
+
+                    @Override
+                    public JSONObject wearCloth(Object pojo, JSONObject jsonObject) {
+                        String domainUrl =
+                                PropConfig.getInstance().getPropertyValue(Constants.STATIC_URL);
+                        try {
+                            if (StringUtil.isNotEmpty(jsonObject.getString("imageUrl"))) {
+                                jsonObject.put("imageUrl",
+                                        domainUrl.concat(jsonObject.getString("imageUrl")));
+                            }
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                        }
+                        return jsonObject;
+                    }
+
+                });
     }
 
     public static class GuahaoLogQuery extends Grid {
