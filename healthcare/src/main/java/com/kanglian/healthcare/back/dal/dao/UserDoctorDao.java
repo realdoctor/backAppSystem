77f65c74df697fe188eace39c40c1ad2afa73a44
liuxl <ylulu2007@sina.com>
2018-06-05 14:25:30 +0800

    用户医生类

---
+++ healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/UserDoctorDao.java
@@ -0,0 +1,8 @@
+package com.kanglian.healthcare.back.dal.dao;
+
+import com.kanglian.healthcare.back.dal.pojo.UserDoctor;
+import com.easyway.business.framework.dao.CrudDao;
+
+public interface UserDoctorDao extends CrudDao<UserDoctor> {
+
+}
