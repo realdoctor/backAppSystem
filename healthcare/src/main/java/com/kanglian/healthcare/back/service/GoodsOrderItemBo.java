e49ea4fb0e57c7e4be16973c170c3bab4a014d8f
liuxl <ylulu2007@sina.com>
2018-06-11 14:36:36 +0800

    clean

--- healthcare/src/main/java/com/kanglian/healthcare/back/service/GoodsOrderItemBo.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/GoodsOrderItemBo.java
@@ -1,11 +1,12 @@
 package com.kanglian.healthcare.back.service;
 
-import com.easyway.business.framework.bo.CrudBo;
-import com.kanglian.healthcare.back.dal.dao.GoodsOrderItemDao;
-import com.kanglian.healthcare.back.dal.pojo.GoodsOrderItem;
-import com.kanglian.healthcare.exception.DBException;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import com.easyway.business.framework.bo.CrudBo;
+import com.kanglian.healthcare.back.common.DaoExecutorAdapter;
+import com.kanglian.healthcare.back.common.DaoTemplate;
+import com.kanglian.healthcare.back.dal.dao.GoodsOrderItemDao;
+import com.kanglian.healthcare.back.dal.pojo.GoodsOrderItem;
 
 @Service
 public class GoodsOrderItemBo extends CrudBo<GoodsOrderItem, GoodsOrderItemDao> {
@@ -16,25 +17,33 @@ public class GoodsOrderItemBo extends CrudBo<GoodsOrderItem, GoodsOrderItemDao> 
      * @param orderNo
      * @return
      */
-    public List<GoodsOrderItem> getGoodsOrderDetailByOrderNo(String orderNo) {
-        try {
-            return this.dao.getGoodsOrderDetailByOrderNo(orderNo);
-        } catch (Exception ex) {
-            throw new DBException(ex);
-        }
+    public List<GoodsOrderItem> getGoodsOrderDetailByOrderNo(final String orderNo) {
+        return DaoTemplate.selectList(new DaoExecutorAdapter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public List<GoodsOrderItem> selectList() throws Exception {
+                return getDao().getGoodsOrderDetailByOrderNo(orderNo);
+            }
+
+        });
     }
-    
+
     /**
      * 订单id获取订单明细
      * 
      * @param goodsOrderId
      * @return
      */
-    public List<GoodsOrderItem> getGoodsOrderDetail(String goodsOrderId){
-        try {
-            return this.dao.getGoodsOrderDetail(Integer.valueOf(goodsOrderId));
-        } catch (Exception ex) {
-            throw new DBException(ex);
-        }
+    public List<GoodsOrderItem> getGoodsOrderDetail(final String goodsOrderId) {
+        return DaoTemplate.selectList(new DaoExecutorAdapter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public List<GoodsOrderItem> selectList() throws Exception {
+                return getDao().getGoodsOrderDetail(Integer.valueOf(goodsOrderId));
+            }
+            
+        });
     }
 }
