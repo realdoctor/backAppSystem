67eec9ff49680b11083fe20cac8ac66a51e4ad80
liuxl <ylulu2007@sina.com>
2018-06-12 18:13:48 +0800

    搜索加分页

--- healthcare/src/main/java/com/kanglian/healthcare/back/service/HospitalAddressBo.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/HospitalAddressBo.java
@@ -4,6 +4,9 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import com.easyway.business.framework.bo.CrudBo;
 import com.easyway.business.framework.mybatis.query.ConditionQuery;
+import com.easyway.business.framework.pojo.Grid;
+import com.kanglian.healthcare.back.common.DaoExecutorAdapter;
+import com.kanglian.healthcare.back.common.DaoTemplate;
 import com.kanglian.healthcare.back.dal.dao.HospitalAddressDao;
 import com.kanglian.healthcare.back.dal.pojo.HospitalAddress;
 import com.kanglian.healthcare.exception.DBException;
@@ -22,5 +25,23 @@ public class HospitalAddressBo extends CrudBo<HospitalAddress, HospitalAddressDa
         } catch (Exception ex) {
             throw new DBException(ex);
         }
+    }
+    
+    /**
+     * 分页搜索医院、医生、科室、疾病
+     * 
+     * @return 医院 | 医生
+     */
+    public Grid queryForHospitalAndDoctor(final Grid grid) {
+        return DaoTemplate.pagingList(new DaoExecutorAdapter() {
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public List<HospitalAddress> selectList() throws Exception {
+                ConditionQuery query = grid.buildConditionQuery();
+                return getDao().queryForHospitalAndDoctor(query);
+            }
+
+        }, grid.getPageNum(), grid.getPageSize());
     }
 }
