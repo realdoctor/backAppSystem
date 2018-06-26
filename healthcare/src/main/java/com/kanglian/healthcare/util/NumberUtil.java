df20d6b851eb1ec77127a09c4b40946011f4679c
liuxl <ylulu2007@sina.com>
2018-06-26 15:03:33 +0800

    uuid修改

--- healthcare/src/main/java/com/kanglian/healthcare/util/NumberUtil.java
+++ healthcare/src/main/java/com/kanglian/healthcare/util/NumberUtil.java
@@ -33,16 +33,20 @@ public final class NumberUtil {
      * 
      * @return
      */
-    public static String getOrderNo() {
+    public static String getOrderIdByUUId() {
         int hashCodeV = UUID.randomUUID().toString().hashCode();
         if (hashCodeV < 0) {// 有可能是负数
             hashCodeV = -hashCodeV;
         }
+        // 0 代表前面补充0
+        // 4 代表长度为4
+        // d 代表参数为正数型
         String machineId = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
         String orderId = machineId + String.format("%018d", hashCodeV);
+        System.out.println(orderId);
         return orderId;
     }
-
+    
     /**
      * 产生num位的随机数
      * 
