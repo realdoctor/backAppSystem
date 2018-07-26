1f0821ec5d01a7596eda24b5f19a1946b3b593fd
liuxl <ylulu2007@sina.com>
2018-07-26 10:47:22 +0800

    1

--- healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/AskQuestionAnswerDao.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/AskQuestionAnswerDao.java
@@ -11,16 +11,23 @@ public interface AskQuestionAnswerDao extends NewCrudDao<AskQuestionAnswer> {
     public List<AskQuestionAnswer> getListByMessageId(String messageId);
 
     /**
-     * 获取患者超过三天未处理已回复列表
+     * 获取超过三天未处理，进行中列表
      * 
      * @return
      */
     public List<AskQuestionAnswer> getListOverThreeday();
 
     /**
-     * 更新患者超过三天未处理，已回复列表状态2
+     * 更新超过三天未处理，进行中列表状态2
      * 
      * @return
      */
     public int updateStatusOverThreeday();
+    
+    /**
+     * 超过三天未处理，未回复列表[退款]
+     * 
+     * @return
+     */
+    public List<AskQuestionAnswer> getListOverThreedayUnAnswer();
 }
