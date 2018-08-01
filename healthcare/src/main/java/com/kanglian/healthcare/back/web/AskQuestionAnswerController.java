e3e5ba0ac55eb8a5700899ea1de7f3627adc5c92
liuxl <ylulu2007@sina.com>
2018-08-01 14:15:37 +0800

    加入角色判断

--- healthcare/src/main/java/com/kanglian/healthcare/back/web/AskQuestionAnswerController.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/web/AskQuestionAnswerController.java
@@ -22,11 +22,13 @@ import com.kanglian.healthcare.back.constants.Constants;
 import com.kanglian.healthcare.back.dal.pojo.AskQuestionAnswer;
 import com.kanglian.healthcare.back.dal.pojo.User;
 import com.kanglian.healthcare.back.dal.pojo.UserInfo;
+import com.kanglian.healthcare.back.dal.pojo.UserRole;
 import com.kanglian.healthcare.back.jpush.JPushService;
 import com.kanglian.healthcare.back.jpush.PushModel;
 import com.kanglian.healthcare.back.service.AskQuestionAnswerBo;
 import com.kanglian.healthcare.back.service.UserBo;
 import com.kanglian.healthcare.back.service.UserInfoBo;
+import com.kanglian.healthcare.back.service.UserRoleBo;
 import com.kanglian.healthcare.exception.InvalidParamException;
 
 /**
@@ -41,6 +43,8 @@ public class AskQuestionAnswerController extends CrudController<AskQuestionAnswe
 
     @Autowired
     private UserBo       userBo;
+    @Autowired
+    private UserRoleBo   userRoleBo;
     @Autowired
     private UserInfoBo   userInfoBo;
     @Autowired
@@ -145,45 +149,50 @@ public class AskQuestionAnswerController extends CrudController<AskQuestionAnswe
         if (StringUtil.isEmpty(questionId)) {
             throw new InvalidParamException("questionId");
         }
-
+        
+        UserRole userRole = userRoleBo.get(Integer.valueOf(userId));
+        if (userRole != null && userRole.getRoleId() == 0) {
+            String retryNum = query.getRetryNum();
+            if (StringUtil.isEmpty(retryNum)) {
+                throw new InvalidParamException("retryNum");
+            }
+        }
         AskQuestionAnswer askQuestionAnswer = this.bo.get(Long.valueOf(questionId));
         if (askQuestionAnswer == null) {
             return ResultUtil.error("请求回复不存在");
         }
-        if (askQuestionAnswer != null) {
+        if (StringUtil.isNotEmpty(content)) {
             askQuestionAnswer.setAnswer(content);
-            if (StringUtil.isNotEmpty(content)) {
+            askQuestionAnswer.setLastUpdateDtime(DateUtil.currentDate());
+            this.bo.update(askQuestionAnswer);
+            try {
+                User u = userBo.get(userId);
+                PushModel pushModel = new PushModel();
+                pushModel.setTitle("复诊");
+                pushModel.setContent(u.getRealName() + "回复了【"+askQuestionAnswer.getTitle()+"】问题。");
+                pushModel.addParam(Constants.TAG_ID, Constants.TAG_PATIENT_ID);
+                pushModel.addAlias(askQuestionAnswer.getUserId()+"");
+                jPushService.pushToAll(pushModel);
+                logger.info("======================"+pushModel.getContent()+"患者用户userId="+askQuestionAnswer.getUserId());
+            } catch (Exception e) {
+                // TODO: handle exception
+                logger.info("【回复问题】极光推送异常", e);
+            }
+        } else {
+            // 医生未回答三天后退款
+            // 如果医生回复了，患者查看记录。计时3天到期时间取[更新时间]，否则取[创建时间]进行判断
+            // 患者发问第二次问题，上一次的问题就结束
+            if (askQuestionAnswer.getLastUpdateDtime() != null
+                    && StringUtil.isNotEmpty(askQuestionAnswer.getAnswer())) {
                 askQuestionAnswer.setLastUpdateDtime(DateUtil.currentDate());
                 this.bo.update(askQuestionAnswer);
-                try {
-                    User u = userBo.get(userId);
-                    PushModel pushModel = new PushModel();
-                    pushModel.setTitle("复诊");
-                    pushModel.setContent(u.getRealName() + "回复了【"+askQuestionAnswer.getTitle()+"】问题。");
-                    pushModel.addParam(Constants.TAG_ID, Constants.TAG_PATIENT_ID);
-                    pushModel.addAlias(askQuestionAnswer.getUserId()+"");
-                    jPushService.pushToAll(pushModel);
-                    logger.info("======================"+pushModel.getContent()+"患者用户userId="+askQuestionAnswer.getUserId());
-                } catch (Exception e) {
-                    // TODO: handle exception
-                    logger.info("【回复问题】极光推送异常", e);
-                }
-            } else {
-                // 医生未回答三天后退款
-                // 如果医生回复了，患者查看记录。计时3天到期时间取[更新时间]，否则取[创建时间]进行判断
-                // 患者发问第二次问题，上一次的问题就结束
-                if (askQuestionAnswer.getLastUpdateDtime() != null
-                        && StringUtil.isNotEmpty(askQuestionAnswer.getAnswer())) {
-                    askQuestionAnswer.setLastUpdateDtime(DateUtil.currentDate());
-                    this.bo.update(askQuestionAnswer);
-                }
-                
-                // 三次问诊机会已用完，则更新为已结束。
-                if (StringUtil.isNotEmpty(query.getRetryNum()) && "0".equals(query.getRetryNum())) {
-                    askQuestionAnswer.setStatus("2");
-                    askQuestionAnswer.setLastUpdateDtime(DateUtil.currentDate());
-                    this.bo.update(askQuestionAnswer);
-                }
+            }
+            
+            // 三次问诊机会已用完，则更新为已结束。
+            if (StringUtil.isNotEmpty(query.getRetryNum()) && "0".equals(query.getRetryNum())) {
+                askQuestionAnswer.setStatus("2");
+                askQuestionAnswer.setLastUpdateDtime(DateUtil.currentDate());
+                this.bo.update(askQuestionAnswer);
             }
         }
         return ResultUtil.success();
