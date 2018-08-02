26ef9c6250416881fe3cff871bd97a60f806fa8a
liuxl <ylulu2007@sina.com>
2018-08-02 13:39:09 +0800

    1

--- healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/HospitalRegisterDoctorDao.java
+++
@@ -1,8 +0,0 @@
-package com.kanglian.healthcare.back.dal.dao;
-
-import com.kanglian.healthcare.back.dal.pojo.HospitalRegisterDoctor;
-import com.easyway.business.framework.dao.CrudDao;
-
-public interface HospitalRegisterDoctorDao extends CrudDao<HospitalRegisterDoctor> {
-
-}
