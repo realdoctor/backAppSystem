472c423c294f718b43458a84783eb4734d26e6d4
liuxl <ylulu2007@sina.com>
2018-08-07 11:04:59 +0800

    修改问诊管理

--- healthcare/src/main/java/com/kanglian/healthcare/back/web/AskQuestionAnswerController.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/web/AskQuestionAnswerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSONObject;
 import com.easyway.business.framework.json.JsonClothProcessor;
+import com.easyway.business.framework.mybatis.annotion.SingleValue;
 import com.easyway.business.framework.mybatis.query.ConditionQuery;
 import com.easyway.business.framework.mybatis.query.condition.SingleValueCondition;
 import com.easyway.business.framework.mybatis.query.condition.WithoutValueCondition;
@@ -65,8 +66,8 @@ public class AskQuestionAnswerController extends CrudController<AskQuestionAnswe
         if (StringUtil.isEmpty(query.getRoleId())) {
             throw new InvalidParamException("roleId");
         }
-        if (StringUtil.isEmpty(query.getType())) {
-            throw new InvalidParamException("type");
+        if (StringUtil.isEmpty(query.getStatus())) {
+            throw new InvalidParamException("status");
         }
         
         logger.info("[我的复诊]患者病历列表，roleId=" + query.getRoleId());
@@ -88,55 +89,6 @@ public class AskQuestionAnswerController extends CrudController<AskQuestionAnswe
             
         });
     }
-    
-//    /**
-//     * 回复内容
-//     * 
-//     * @param messageId
-//     * @return
-//     * @throws Exception
-//     */
-//    @PostMapping("/reply")
-//    public ResultBody reply(@RequestBody AskQuestionQuery query) throws Exception {
-//        String userId = query.getUserId();
-//        String roleId = query.getRoleId();
-//        String questionId = query.getMessageId();
-//        String content = query.getContent();
-//        if (StringUtil.isEmpty(userId)) {
-//            throw new InvalidParamException("userId");
-//        }
-//        if (StringUtil.isEmpty(roleId)) {
-//            throw new InvalidParamException("roleId");
-//        }
-//        if (StringUtil.isEmpty(content)) {
-//            throw new InvalidParamException("content");
-//        }
-//        if (StringUtil.isEmpty(questionId)) {
-//            throw new InvalidParamException("questionId");
-//        }
-//
-//        AskQuestionAnswer askQuestionAnswer = this.bo.get(Long.valueOf(questionId));
-//        if (askQuestionAnswer == null) {
-//            return ResultUtil.error("请求回复非法");
-//        }
-//        if ("1".equals(roleId)) {// 医生回复，更新列表
-//            if (askQuestionAnswer != null) {
-//                askQuestionAnswer.setAnswer(content);
-//                askQuestionAnswer.setLastUpdateDtime(DateUtil.currentDate());
-//                this.bo.update(askQuestionAnswer);
-//            }
-//        } else {
-//            // 患者回复，插入新问题
-//            AskQuestionAnswer newAskQuestionAnswer = new AskQuestionAnswer();
-//            newAskQuestionAnswer.setUserId(Integer.valueOf(userId));
-//            newAskQuestionAnswer.setMessageId(askQuestionAnswer.getMessageId());
-//            newAskQuestionAnswer.setToUser(askQuestionAnswer.getToUser());
-//            newAskQuestionAnswer.setQuestion(content);
-//            newAskQuestionAnswer.setAddTime(DateUtil.currentDate());
-//            this.bo.save(newAskQuestionAnswer);
-//        }
-//        return ResultUtil.success();
-//    }
     
     /**
      * 患者医生回复问诊
@@ -221,68 +173,16 @@ public class AskQuestionAnswerController extends CrudController<AskQuestionAnswe
         return ResultUtil.success(this.bo.getListByMessageId(messageId));
     }
     
-//    /**
-//     * 回复内容详情
-//     * 
-//     * @param messageId
-//     * @return
-//     * @throws Exception
-//     */
-//    @GetMapping("/reply/info")
-//    public ResultBody info(String messageId) throws Exception {
-//        if (StringUtil.isEmpty(messageId)) {
-//            throw new InvalidParamException("messageId");
-//        }
-//        
-//        final List<AskQuestionAnswer> resultList = this.bo.getListByMessageId(messageId);
-//        if (CollectionUtil.isNotEmpty(resultList)) {
-//            AskQuestionAnswer entity = resultList.get(0);// 获取最新一条，未回答三天后打款
-//            if (StringUtil.isEmpty(entity.getAnswer()) 
-//                    && entity.getLastUpdateDtime() == null) {
-//                // 默认点开第一次就算应答
-//                entity.setLastUpdateDtime(DateUtil.currentDate());
-//                this.bo.update(entity);
-//            }
-//        }
-//        
-//        List<JSONObject> jsonObj = ResultUtil.wearCloth(resultList, new JsonClothProcessor() {
-//
-//            private AskQuestionAnswer maxQuestionQuestionAnswer = Collections.max(resultList);
-//
-//            @Override
-//            public JSONObject wearCloth(Object pojo, JSONObject jsonObject) {
-//                try {
-//                    // 取同一，主要是为了。在回复时更新
-//                    if (maxQuestionQuestionAnswer.getId() != null) {
-//                        jsonObject.put("questionId", maxQuestionQuestionAnswer.getId());
-//                    }
-//                } catch (Exception e) {
-//                    // TODO: handle exception
-//                }
-//                return jsonObject;
-//            }
-//
-//        });
-//        return ResultUtil.success(jsonObj);
-//    }
-    
     public static class AskQuestionQuery extends Grid {
 
         private String userId;
         private String roleId;
+        // 1|进行中，2|已完结
+        private String status;
+        private String realName;
         private String questionId;
         private String content;
-        // 1|进行中，2|已完结
-        private String type;
         private String retryNum;
-
-        public String getRetryNum() {
-            return retryNum;
-        }
-
-        public void setRetryNum(String retryNum) {
-            this.retryNum = retryNum;
-        }
 
         public String getUserId() {
             return userId;
@@ -298,6 +198,31 @@ public class AskQuestionAnswerController extends CrudController<AskQuestionAnswe
 
         public void setRoleId(String roleId) {
             this.roleId = roleId;
+        }
+        
+        @SingleValue(tableAlias="t", column = "status", equal = "=")
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getRealName() {
+            return realName;
+        }
+
+        public void setRealName(String realName) {
+            this.realName = realName;
+        }
+        
+        public String getRetryNum() {
+            return retryNum;
+        }
+
+        public void setRetryNum(String retryNum) {
+            this.retryNum = retryNum;
         }
 
         public String getQuestionId() {
@@ -316,33 +241,28 @@ public class AskQuestionAnswerController extends CrudController<AskQuestionAnswe
             this.content = content;
         }
 
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
         @Override
         public ConditionQuery buildConditionQuery() {
             ConditionQuery query = super.buildConditionQuery();
             if ("1".equals(getRoleId())) {// 医生1
+                query.addParam("roleId", "1");
                 query.addSingleValueCondition(new SingleValueCondition("to_user", getUserId()));
+                if (StringUtil.isNotEmpty(getRealName())) {
+                    query.addWithoutValueCondition(new WithoutValueCondition("u1.real_name LIKE '%"+getRealName()+"%'"));
+                }
             } else {// 普通用户0
+                query.addParam("roleId", "0");
                 query.addSingleValueCondition(new SingleValueCondition("user_id", getUserId()));
             }
-            if ("2".equals(getType())) {// 已结束
-                query.addParam("type", "2");
-//                query.addWithoutValueCondition(
-//                        new WithoutValueCondition(" IF((t.last_update_dtime IS NOT NULL AND t.last_update_dtime <> ''), datediff(now(), t.last_update_dtime) > 3, datediff(now(), t.add_time) > 3) OR t.status=2 "));
-                query.addWithoutValueCondition(new WithoutValueCondition(" t.status=2 "));
-            } else if ("1".equals(getType())) {// 进行中
-                query.addParam("type", "1");
-//                query.addWithoutValueCondition(new WithoutValueCondition(
-//                        " IF((t.last_update_dtime IS NOT NULL AND t.last_update_dtime <> ''), datediff(now(), t.last_update_dtime) <= 3, datediff(now(), t.add_time) <= 3) OR t.status=1 "));
-                query.addWithoutValueCondition(new WithoutValueCondition(" t.status=1 "));
-            }
+//            if ("2".equals(getStatus())) {// 已结束
+////                query.addWithoutValueCondition(
+////                        new WithoutValueCondition(" IF((t.last_update_dtime IS NOT NULL AND t.last_update_dtime <> ''), datediff(now(), t.last_update_dtime) > 3, datediff(now(), t.add_time) > 3) OR t.status=2 "));
+//                query.addWithoutValueCondition(new WithoutValueCondition(" t.status=2 "));
+//            } else if ("1".equals(getStatus())) {// 进行中
+////                query.addWithoutValueCondition(new WithoutValueCondition(
+////                        " IF((t.last_update_dtime IS NOT NULL AND t.last_update_dtime <> ''), datediff(now(), t.last_update_dtime) <= 3, datediff(now(), t.add_time) <= 3) OR t.status=1 "));
+//                query.addWithoutValueCondition(new WithoutValueCondition(" t.status=1 "));
+//            }
             return query;
         }
     }
