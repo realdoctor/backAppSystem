56cdc55b59894195a2a71943984d456971e139f3
liuxl <ylulu2007@sina.com>
2018-08-16 10:11:41 +0800

    日志输出分类

--- healthcare/src/main/java/com/kanglian/healthcare/util/LogUtil.java
+++ healthcare/src/main/java/com/kanglian/healthcare/util/LogUtil.java
@@ -11,18 +11,25 @@ import org.slf4j.LoggerFactory;
 public final class LogUtil {
     /** logger */
     public static final String  LOG_NAME_SYS     = "HealthCare.Sys";
-    public static final String  LOG_NAME_PAYMENT = "HealthCare.Pay";
+    public static final String  LOG_NAME_MESSAGE = "HealthCare.Message";
     public static final String  LOG_NAME_TASK    = "HealthCare.Task";
+    public static final String  LOG_NAME_PAYMENT = "HealthCare.Pay";
 
+    private static final Logger message;
     private static final Logger pay;
     private static final Logger task;
 
     static {
+        message = LoggerFactory.getLogger(LOG_NAME_MESSAGE);
         pay = LoggerFactory.getLogger(LOG_NAME_PAYMENT);
         task = LoggerFactory.getLogger(LOG_NAME_TASK);
 
+        if (message == null) {
+            System.out.println("WARNING: Can not get message logger.");
+        }
+
         if (pay == null) {
-            System.out.println("WARNING: Can not get pay logger.");
+            System.out.println("WARNING: Can not get payment logger.");
         }
 
         if (task == null) {
@@ -38,6 +45,15 @@ public final class LogUtil {
      * 获取消息输出Log
      * 
      * @return 消息输出Log
+     */
+    public static Logger getMessageLogger() {
+        return message;
+    }
+
+    /**
+     * 获取支付输出Log
+     * 
+     * @return 支付输出Log
      */
     public static Logger getPayLogger() {
         return pay;
