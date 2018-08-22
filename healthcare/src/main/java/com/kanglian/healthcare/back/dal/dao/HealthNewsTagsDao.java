e97600ff32fc193670920e6c03c9482d9a33d549
liuxl <ylulu2007@sina.com>
2018-08-22 16:35:42 +0800

    1

--- healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/HealthNewsTagsDao.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/HealthNewsTagsDao.java
@@ -1,9 +1,10 @@
 package com.kanglian.healthcare.back.dal.dao;
 
 import com.kanglian.healthcare.back.dal.pojo.HealthNewsTags;
+import java.util.List;
 import com.easyway.business.framework.dao.CrudDao;
 
 public interface HealthNewsTagsDao extends CrudDao<HealthNewsTags> {
 
-    public HealthNewsTags getByNewsId(Integer newsId);
+    public List<HealthNewsTags> getByNewsId(Integer newsId);
 }
