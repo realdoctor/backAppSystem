3a0d8cd669338ffb00fae7a4cb6a2fb9213ef6b9
liuxl <ylulu2007@sina.com>
2018-08-24 16:34:28 +0800

    医生可查看自身收益情况

--- healthcare/src/main/java/com/kanglian/healthcare/back/web/PaymentLogController.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/web/PaymentLogController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSONObject;
 import com.easyway.business.framework.json.JsonClothProcessor;
 import com.easyway.business.framework.mybatis.annotion.SingleValue;
+import com.easyway.business.framework.mybatis.query.ConditionQuery;
+import com.easyway.business.framework.mybatis.query.condition.SingleValueCondition;
+import com.easyway.business.framework.mybatis.query.condition.WithoutValueCondition;
 import com.easyway.business.framework.pojo.Grid;
 import com.easyway.business.framework.springmvc.controller.CrudController;
 import com.easyway.business.framework.springmvc.result.ResultBody;
 import com.easyway.business.framework.util.StringUtil;
 import com.kanglian.healthcare.authorization.annotation.Authorization;
+import com.kanglian.healthcare.authorization.annotation.CurrentUser;
 import com.kanglian.healthcare.back.constants.Constants;
 import com.kanglian.healthcare.back.dal.pojo.PaymentLog;
+import com.kanglian.healthcare.back.dal.pojo.User;
 import com.kanglian.healthcare.back.service.PaymentLogBo;
 import com.kanglian.healthcare.exception.InvalidParamException;
 import com.kanglian.healthcare.util.PropConfig;
@@ -24,10 +29,12 @@ public class PaymentLogController
         extends CrudController<PaymentLog, PaymentLogBo> {
 
     @GetMapping("/payment/list")
-    public ResultBody list(PaymentIncomeQuery query) throws Exception {
+    public ResultBody list(@CurrentUser User user, PaymentIncomeQuery query) throws Exception {
         if (StringUtil.isEmpty(query.getUserId())) {
             throw new InvalidParamException("userId");
         }
+        query.setUserId(user.getUserId()+"");
+        query.setRoleId(user.getRoleId()+"");
         return super.list(query, new JsonClothProcessor() {
 
             String domainUrl = PropConfig.getInstance().getPropertyValue(Constants.STATIC_URL);
@@ -53,9 +60,10 @@ public class PaymentLogController
 
     public static class PaymentIncomeQuery extends Grid {
         private String userId;
+        private String roleId;
         private String mark;
 
-        @SingleValue(column = "user_id", equal = "=")
+//        @SingleValue(column = "user_id", equal = "=")
         public String getUserId() {
             return userId;
         }
@@ -64,6 +72,14 @@ public class PaymentLogController
             this.userId = userId;
         }
 
+        public String getRoleId() {
+            return roleId;
+        }
+
+        public void setRoleId(String roleId) {
+            this.roleId = roleId;
+        }
+        
         @SingleValue(column = "mark", equal = "=")
         public String getMark() {
             return mark;
@@ -72,5 +88,21 @@ public class PaymentLogController
         public void setMark(String mark) {
             this.mark = mark;
         }
+
+        @Override
+        public ConditionQuery buildConditionQuery() {
+            ConditionQuery query = super.buildConditionQuery();
+            if (StringUtil.isNotBlank(roleId)) {
+                if ("1".equals(roleId)) {// 医生用户，查看收入
+                    query.addWithoutValueCondition(new WithoutValueCondition(
+                            " t.user_id = " + userId + " OR (t.to_user = " + userId + " AND t.mark !=2)"));
+                } else {
+                    query.addSingleValueCondition(new SingleValueCondition("user_id", userId));
+                }
+            }
+
+            return query;
+        }
+        
     }
 }
