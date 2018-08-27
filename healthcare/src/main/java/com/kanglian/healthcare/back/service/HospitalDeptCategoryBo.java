bd28e66aede4fae1bc112666d2f7c9b9094d46bd
liuxl <ylulu2007@sina.com>
2018-08-27 14:13:45 +0800

    更新科室码

--- healthcare/src/main/java/com/kanglian/healthcare/back/service/HospitalDeptCategoryBo.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/HospitalDeptCategoryBo.java
@@ -1,15 +1,22 @@
 package com.kanglian.healthcare.back.service;
 
 import java.util.List;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.easyway.business.framework.bo.CrudBo;
 import com.kanglian.healthcare.back.common.DaoExecutorAdapter;
 import com.kanglian.healthcare.back.common.DaoTemplate;
 import com.kanglian.healthcare.back.dal.dao.HospitalDeptCategoryDao;
 import com.kanglian.healthcare.back.dal.pojo.HospitalDeptCategory;
+import com.kanglian.healthcare.back.dal.pojo.HospitalDeptCategoryItem;
 
 @Service
 public class HospitalDeptCategoryBo extends CrudBo<HospitalDeptCategory, HospitalDeptCategoryDao> {
+    
+    @Autowired
+    private HospitalDeptCategoryItemBo hospitalDeptCategoryItemBo;
+    
     /**
      * 查询医疗机构各科室分类表
      * 
@@ -38,5 +45,29 @@ public class HospitalDeptCategoryBo extends CrudBo<HospitalDeptCategory, Hospita
             }
 
         });
+    }
+    
+    public void testUpdateDeptCode() {
+        List<HospitalDeptCategory> deptCategoryList = this.queryAll();
+        for (HospitalDeptCategory deptCategory : deptCategoryList) {
+            String sss = RandomStringUtils.randomAlphanumeric(16);
+            deptCategory.setDeptCode("DEPT"+sss);
+            this.dao.update(deptCategory);
+        }
+
+        List<HospitalDeptCategoryItem> deptCategoryItemList = hospitalDeptCategoryItemBo.queryAll();
+        for (HospitalDeptCategoryItem deptCategoryItem : deptCategoryItemList) {
+            int retryNum = 0;
+            do {
+                try {
+                    String sss = RandomStringUtils.randomAlphanumeric(16);
+                    deptCategoryItem.setDeptCodeNew("DPT-" + sss);
+                    this.hospitalDeptCategoryItemBo.updateNew(deptCategoryItem);
+                    retryNum = 1;
+                } catch (Exception e) {
+                    retryNum = 0;
+                }
+            } while (retryNum <= 0);
+        }
     }
 }
--- healthcare/src/main/java/com/kanglian/healthcare/back/service/HospitalDeptCategoryItemBo.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/service/HospitalDeptCategoryItemBo.java
@@ -2,10 +2,21 @@ package com.kanglian.healthcare.back.service;
 
 import com.easyway.business.framework.bo.CrudBo;
 import org.springframework.stereotype.Service;
+import com.kanglian.healthcare.back.common.DaoExecutorAdapter;
+import com.kanglian.healthcare.back.common.DaoTemplate;
 import com.kanglian.healthcare.back.dal.dao.HospitalDeptCategoryItemDao;
 import com.kanglian.healthcare.back.dal.pojo.HospitalDeptCategoryItem;
 
 @Service
-public class HospitalDeptCategoryItemBo extends CrudBo<HospitalDeptCategoryItem,HospitalDeptCategoryItemDao> {
+public class HospitalDeptCategoryItemBo extends CrudBo<HospitalDeptCategoryItem, HospitalDeptCategoryItemDao> {
+    public void updateNew(final HospitalDeptCategoryItem pojo) {
+        DaoTemplate.execute(new DaoExecutorAdapter() {
 
+            @Override
+            public void execute() throws Exception {
+                getDao().updateNew(pojo);
+            }
+
+        });
+    }
 }
