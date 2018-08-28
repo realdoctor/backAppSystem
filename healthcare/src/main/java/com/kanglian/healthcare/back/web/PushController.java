3ed687ce965b9ff51b681ab08e5315f2a5ac28ac
liuxl <ylulu2007@sina.com>
2018-08-28 16:42:11 +0800

    推送修改

--- healthcare/src/main/java/com/kanglian/healthcare/back/web/PushController.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/web/PushController.java
@@ -1,11 +1,12 @@
 package com.kanglian.healthcare.back.web;
 
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.easyway.business.framework.pojo.QueryPojo;
 import com.easyway.business.framework.springmvc.result.ResultBody;
 import com.easyway.business.framework.springmvc.result.ResultUtil;
 import com.easyway.business.framework.util.StringUtil;
@@ -13,8 +14,10 @@ import com.kanglian.healthcare.authorization.annotation.Authorization;
 import com.kanglian.healthcare.authorization.annotation.CurrentUser;
 import com.kanglian.healthcare.back.constants.Constants;
 import com.kanglian.healthcare.back.dal.pojo.User;
+import com.kanglian.healthcare.back.dal.pojo.UserRole;
 import com.kanglian.healthcare.back.jpush.JPushService;
 import com.kanglian.healthcare.back.jpush.PushModel;
+import com.kanglian.healthcare.back.service.UserRoleBo;
 import com.kanglian.healthcare.exception.InvalidParamException;
 import com.kanglian.healthcare.util.LogUtil;
 
@@ -29,16 +32,18 @@ public class PushController extends BaseController {
 
     @Autowired
     private JPushService jPushService;
+    @Autowired
+    private UserRoleBo userRoleBo;
 
     @Authorization
     @RequestMapping(value = "/pushmsg", method = RequestMethod.POST)
-    public @ResponseBody ResultBody pushmsg(@CurrentUser User user, HttpServletRequest request) throws Exception {
-        String receiveId = request.getParameter("receiveId");
+    public @ResponseBody ResultBody pushmsg(@CurrentUser User user, @RequestBody PushmsgQuery query) throws Exception {
+        String receiveId = query.getReceiveId();
         if (StringUtil.isEmpty(receiveId) || "null".equals(receiveId)) {
             throw new InvalidParamException("receiveId");
         }
 
-        String content = request.getParameter("content");
+        String content = query.getContent();
         if (StringUtil.isEmpty(content) || "null".equals(content)) {
             throw new InvalidParamException("content");
         }
@@ -46,12 +51,37 @@ public class PushController extends BaseController {
         PushModel pushModel = new PushModel();
         pushModel.setTitle("即时聊天");
         pushModel.setContent(content);
-        pushModel.addParam(Constants.TAG_ID, Constants.TAG_DOCTOR_ID);
         pushModel.addAlias(receiveId);
+        try {
+            UserRole u = userRoleBo.get(Integer.valueOf(receiveId));
+            if (u != null && u.getRoleId() == 1) {
+                pushModel.addParam(Constants.TAG_ID, Constants.TAG_DOCTOR_ID);
+            } else {
+                pushModel.addParam(Constants.TAG_ID, Constants.TAG_PATIENT_ID);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
         jPushService.pushToAndroid(pushModel);
         LogUtil.getMessageLogger().info("【即时聊天】发送用户userId={}，接收用户receiveUserId={}, 内容={}",
                 new Object[] {user.getUserId(), receiveId, content});
         return ResultUtil.success();
     }
 
+    public static class PushmsgQuery extends QueryPojo {
+        private String receiveId;
+        private String content;
+        public String getReceiveId() {
+            return receiveId;
+        }
+        public void setReceiveId(String receiveId) {
+            this.receiveId = receiveId;
+        }
+        public String getContent() {
+            return content;
+        }
+        public void setContent(String content) {
+            this.content = content;
+        }
+    }
 }
