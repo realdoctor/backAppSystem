1f1660b57f1e7a1265f82ebdd7140e73d812e942
liuxl <ylulu2007@sina.com>
2018-08-28 17:03:56 +0800

    getByPatientDiagId加入

--- healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/PatientDrugDao.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/dal/dao/PatientDrugDao.java
@@ -1,8 +1,10 @@
 package com.kanglian.healthcare.back.dal.dao;
 
+import java.util.List;
 import com.easyway.business.framework.dao.CrudDao;
 import com.kanglian.healthcare.back.dal.pojo.PatientDrug;
 
 public interface PatientDrugDao extends CrudDao<PatientDrug> {
 
+    public List<PatientDrug> getByPatientDiagId(Integer patientDiagId);
 }
