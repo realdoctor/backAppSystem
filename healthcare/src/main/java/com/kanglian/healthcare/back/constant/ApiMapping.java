a1d6de17450884685d379429902941098c4ed85b
liuxl <ylulu2007@sina.com>
2018-08-30 17:58:56 +0800

    1

--- healthcare/src/main/java/com/kanglian/healthcare/back/constant/ApiMapping.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/constant/ApiMapping.java
@@ -11,47 +11,47 @@ public final class ApiMapping {
     // ============用户管理
     // ==========================================================================
     /**
-     * 用户注册
+     * <H5>用户注册</H5>
      */
     public final static String USER_REGIST                     = "/user/regist";
     /**
-     * 用户登录
+     * <H5>用户登录</H5>
      */
     public final static String USER_LOGIN                      = "/user/login";
     /**
-     * 用户退出
+     * <H5>用户退出</H5>
      */
     public final static String USER_LOGOUT                     = "/user/logout";
     /**
-     * 手机验证码
+     * <H5>手机验证码</H5>
      */
     public final static String USER_SENDCODE                   = "/user/sendCode";
     /**
-     * 密码修改
+     * <H5>密码修改</H5>
      */
     public final static String USER_UPDATEPWD                  = "/user/updatePwd";
     /**
-     * 忘记（重置）密码
+     * <H5>忘记（重置）密码</H5>
      */
     public final static String USER_RESETPWD                   = "/user/resetPwd";
     /**
-     * 实名认证
+     * <H5>实名认证</H5>
      */
     public final static String USER_IDENTIFY                   = "/user/certification";
     /**
-     * 检查是否已实名
+     * <H5>检查是否已实名</H5>
      */
     public final static String USER_IDENTIFY_CHECK             = "/user/certification/check";
     /**
-     * 刷新token
+     * <H5>刷新token</H5>
      */
     public final static String USER_REFRESH_TOKEN              = "/user/refreshToken";
     /**
-     * 扫描关注医生
+     * <H5>扫描关注医生</H5>
      */
     public final static String USER_MYDOCTOR_ADD               = "/user/mydoctor/add";
     /**
-     * 关注医生列表
+     * <H5>关注医生列表</H5>
      */
     public final static String USER_MYDOCTOR_LIST              = "/user/mydoctor/list";
 
@@ -59,15 +59,15 @@ public final class ApiMapping {
     // ============上传信息
     // ==========================================================================
     /**
-     * 上传头像
+     * <H5>上传头像</H5>
      */
     public final static String UPLOAD_IMG                      = "/upload/uploadImg";
     /**
-     * 我的复诊-上传病历
+     * <H5>我的复诊-上传病历</H5>
      */
     public final static String UPLOAD_PATIENT                  = "/upload/uploadPatient";
     /**
-     * 上传本地病历
+     * <H5>上传本地病历</H5>
      */
     public final static String UPLOAD_PATIENT_RECORD           = "/upload/uploadPatientRecord";
 
@@ -75,11 +75,11 @@ public final class ApiMapping {
     // ============首页
     // ==========================================================================
     /**
-     * 提醒信息内容
+     * <H5>提醒信息内容</H5>
      */
     public final static String USER_MESSAGE_LIST               = "/user/message";
     /**
-     * 提醒信息列表
+     * <H5>提醒信息列表</H5>
      */
     public final static String USER_MESSAGE_NOTICELIST         = "/user/message/noticeList";
 
@@ -87,27 +87,27 @@ public final class ApiMapping {
     // ============患者功能
     // ==========================================================================
     /**
-     * 病历归档-查看病历列表
+     * <H5>病历归档-查看病历列表</H5>
      */
     public final static String PATIENT_LIST                    = "/patient/list";
     /**
-     * 病历归档-病历用药列表
+     * <H5>病历归档-病历用药列表</H5>
      */
     public final static String PATIENT_DRUG_LIST               = "/patient/drug";
     /**
-     * 在线复诊-复诊病历列表
+     * <H5>在线复诊-复诊病历列表</H5>
      */
     public final static String PATIENT_REVISIT_LIST            = "/patient/revisit/list";
     /**
-     * 在线复诊-复诊病历病种列表
+     * <H5>在线复诊-复诊病历病种列表</H5>
      */
     public final static String PATIENT_REVISIT_DIAG_LIST       = "/patient/revisit/diagList";
     /**
-     * 我的复诊-患者回复
+     * <H5>我的复诊-患者回复</H5>
      */
     public final static String ASKQUESTION_REPLY               = "/askQuestion/reply";
     /**
-     * 我的复诊-患者病历列表
+     * <H5>我的复诊-患者病历列表</H5>
      */
     public final static String ASKQUESTION_REPLY_DOCTORLIST    = "/askQuestion/reply/doctorList";
 
@@ -115,19 +115,19 @@ public final class ApiMapping {
     // ============医生功能
     // ==========================================================================
     /**
-     * 我的复诊-患者病历列表
+     * <H5>我的复诊-患者病历列表</H5>
      */
     public final static String ASKQUESTION_REPLY_PATIENTLIST   = "/askQuestion/reply/patientList";
     /**
-     * 我的复诊-患者病历详情
+     * <H5>我的复诊-患者病历详情</H5>
      */
     public final static String ASKQUESTION_REPLY_INFO          = "/askQuestion/reply/info";
     /**
-     * 获取问诊收费配置
+     * <H5>获取问诊收费配置</H5>
      */
     public final static String ASKQUESTION_GETMONEY            = "/askQuestion/getAskQuestionMoney";
     /**
-     * 设置问诊收费配置
+     * <H5>设置问诊收费配置</H5>
      */
     public final static String ASKQUESTION_SETMONEY            = "/askQuestion/setAskQuestionMoney";
 
@@ -135,35 +135,35 @@ public final class ApiMapping {
     // ============预约挂号
     // ==========================================================================
     /**
-     * 我的预约-患者挂号一览（患者）
+     * <H5>我的预约-患者挂号一览（患者）</H5>
      */
     public final static String USER_MYGUAHAOORDER              = "/user/myGuahaoOrder";
     /**
-     * 我的预约-患者挂号一览（医生）
+     * <H5>我的预约-患者挂号一览（医生）</H5>
      */
     public final static String USER_MYPATIENTORDER             = "/user/myPatientOrder";
     /**
-     * 挂号医院一览
+     * <H5>挂号医院一览</H5>
      */
     public final static String GUAHAO_HOSPITAL_LIST            = "/guahao/hospital/list";
     /**
-     * 医院科室分类
+     * <H5>医院科室分类</H5>
      */
     public final static String GUAHAO_HOSPITAL_DEPTCATEGORY    = "/guahao/hospital/deptCategory";
     /**
-     * 预约日期列表
+     * <H5>预约日期列表</H5>
      */
     public final static String GUAHAO_HOSPITAL_DUTYDAY         = "/guahao/hospital/orderDate";
     /**
-     * 按专家预约列表
+     * <H5>按专家预约列表</H5>
      */
     public final static String GUAHAO_HOSPITAL_ORDEREXPERT     = "/guahao/hospital/orderExpert";
     /**
-     * 按日期预约列表
+     * <H5>按日期预约列表</H5>
      */
     public final static String GUAHAO_HOSPITAL_ORDERDATEEXPERT = "/guahao/hospital/orderDateExpert";
     /**
-     * 预约挂号
+     * <H5>预约挂号</H5>
      */
     public final static String GUAHAO_FASTORDER                = "/guahao/fastorder";
 
@@ -171,27 +171,27 @@ public final class ApiMapping {
     // ============商城
     // ==========================================================================
     /**
-     * 商品列表
+     * <H5>商品列表</H5>
      */
     public final static String GOODS_LIST                      = "/goods/list";
     /**
-     * 我的购物车
+     * <H5>我的购物车</H5>
      */
     public final static String GOODS_CART_LIST                 = "/goods/cart/list";
     /**
-     * 添加购物车
+     * <H5>添加购物车</H5>
      */
     public final static String GOODS_CART_ADDCARTITEM          = "/goods/cart/addCartItem";
     /**
-     * 清空购物车
+     * <H5>清空购物车</H5>
      */
     public final static String GOODS_CART_CLEARCART            = "/goods/cart/clearCart";
     /**
-     * 删除购物车
+     * <H5>删除购物车</H5>
      */
     public final static String GOODS_CART_DELETECARTITEM       = "/goods/cart/deleteCartItem";
     /**
-     * 取消订单
+     * <H5>取消订单</H5>
      */
     public final static String GOODS_ORDER_CANCELORDER         = "/goods/order/cancelOrder";
 
@@ -199,19 +199,19 @@ public final class ApiMapping {
     // ============图文视频
     // ==========================================================================
     /**
-     * 上传视频图片
+     * <H5>上传视频图片</H5>
      */
     public final static String UPLOAD_UPLOADFILES              = "/upload/uploadFiles";
     /**
-     * 上传视频图片列表
+     * <H5>上传视频图片列表</H5>
      */
     public final static String NEWS_PUB_LIST                   = "/news_pub/list";
     /**
-     * 上传视频图片详情
+     * <H5>上传视频图片详情</H5>
      */
     public final static String NEWS_PUB_INFO                   = "/news_pub/info";
     /**
-     * 上传上传视频图片
+     * <H5>上传上传视频图片</H5>
      */
     public final static String NEWS_PUB_DEL                    = "/news_pub/del";
 
@@ -219,31 +219,31 @@ public final class ApiMapping {
     // ============资讯信息
     // ==========================================================================
     /**
-     * 资讯一览
+     * <H5>资讯一览</H5>
      */
     public final static String NEWS_LIST                       = "/healthnews/list";
     /**
-     * 资讯详情
+     * <H5>资讯详情</H5>
      */
     public final static String NEWS_INFO                       = "/healthnews/info";
     /**
-     * 关注资讯
+     * <H5>关注资讯</H5>
      */
     public final static String NEWS_FOCUS                      = "/healthnews/focus";
     /**
-     * 取消关注资讯
+     * <H5>取消关注资讯</H5>
      */
     public final static String NEWS_FOCUS_OFF                  = "/healthnews/focus/off";
     /**
-     * 我关注的资讯列表
+     * <H5>我关注的资讯列表</H5>
      */
     public final static String NEWS_FOCUS_LIST                 = "/healthnews/myFocusList";
     /**
-     * 广告列表
+     * <H5>广告列表</H5>
      */
     public final static String NEWS_AD_LIST                    = "/healthnews/ad/list";
     /**
-     * 资讯搜索
+     * <H5>资讯搜索</H5>
      */
     public final static String NEWS_SEARCH                     = "/news/search";
 
@@ -251,7 +251,7 @@ public final class ApiMapping {
     // ============极光推送
     // ==========================================================================
     /**
-     * 推送消息
+     * <H5>推送消息</H5>
      */
     public final static String PUSH_PUSHMSG                    = "/push/pushmsg";
 
@@ -259,11 +259,11 @@ public final class ApiMapping {
     // ============支付
     // ==========================================================================
     /**
-     * 拉取预付单-商城
+     * <H5>拉取预付单-商城</H5>
      */
     public final static String PAY_ORDERPAY                    = "/pay/orderPay";
     /**
-     * 拉取预付单
+     * <H5>拉取预付单</H5>
      */
     public final static String PAY_ORDERPAYT                   = "/pay/orderPayT";
 }
