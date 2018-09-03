a43daa306276047b79b6649a726e99c6e8ad9ac4
liuxl <ylulu2007@sina.com>
2018-09-03 17:24:25 +0800

    <!-- 延迟加载 传递两个参数
    　　　　{pMenuId=child_pid,userId=userId}
    
    　　　　 传递查询参数名称=字段名称
    
    　　　　-->

--- healthcare/src/main/java/com/kanglian/healthcare/back/dao/PatientDrugDao.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/dao/PatientDrugDao.java
@@ -1,10 +1,8 @@
 package com.kanglian.healthcare.back.dao;
 
-import java.util.List;
 import com.easyway.business.framework.dao.CrudDao;
 import com.kanglian.healthcare.back.pojo.PatientDrug;
 
 public interface PatientDrugDao extends CrudDao<PatientDrug> {
 
-    public List<PatientDrug> getByPatientDiagId(Integer patientDiagId);
 }
