b97fc8e302b592477f3fd921e7a4d444b2fb6a29
liuxl <ylulu2007@sina.com>
2018-09-05 15:44:30 +0800

    1

--- healthcare/src/main/java/com/kanglian/healthcare/back/constant/OperateStatus.java
+++ healthcare/src/main/java/com/kanglian/healthcare/back/constant/OperateStatus.java
@@ -7,11 +7,11 @@ package com.kanglian.healthcare.back.constant;
  */
 public final class OperateStatus {
     // 未发货
-    public static final String TRADE_GOODS_UNSEND   = "0";
+    public static final String TRADE_GOODS_UNSEND     = "0";
     // 已发货
-    public static final String TRADE_GOODS_SEND     = "1";
+    public static final String TRADE_GOODS_SEND       = "1";
 
     // 状态：1，进行中；2，已结束
-    public static final String STRING_HAVE_CONTINUE = "1";
-    public static final String STRING_HAVE_FINISH   = "2";
+    public static final String STRING_STATUS_CONTINUE = "1";
+    public static final String STRING_STATUS_FINISH   = "2";
 }
